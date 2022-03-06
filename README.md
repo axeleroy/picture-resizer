50f98b4f23f572293150bfd8b79604476c80d2c8
Axel Leroy <axeleroy@users.noreply.github.com>
2022-03-06 22:04:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# picture-resizer.py
+
+A simple, multi-threaded picture resizing tool leveraging ImageMagick
+
+## Installation
+
+Once you have cloned the repository, initialize a virtualenv and install the dependencies:
+```bash
+python3 -m pip install --user virtualenv #Only if you haven't installed virtualenv yet
+python3 -m venv env
+source env/bin/activate
+python3 -m pip install -r requirements.txt
+```
+
+Then edit the first line of `picture-resizer.py` to match the path
+where the project is cloned:
+```bash
+#!/path/to/picture-resizer/venv/bin/python3
+```
+
+Finally, you can either add the file to your `PATH` or setup an `alias` in your
+run commands file (ie. `.bashrc`, `zshrc`, etc.)
+
+## Usage
+
+```
+picture-resizer.py [-h] --output OUTPUT --width WIDTH [--square] files [files ...]
+```
+
+For example, you can resize all JPEGs in a folder (`*.jpg`) to a 2048px (`--width 2048`)
+width while keeping their aspect ratio to the `/tmp/export` folder (`--output /tmp/export`):
+```
+picture-resizer.py --width 2048 --output /tmp/export *.jpg
+```
+
+Or you can resize them to square with a white background by adding `--square`:
+```
+picture-resizer.py --width 2048 --output /tmp/export --square *.jpg
+```
---
+++ picture-resizer.py
@@ -0,0 +1,61 @@
+#!/home/axel/PycharmProjects/picture-resizer/venv/bin/python3
+import argparse
+import os
+import threading
+from math import ceil
+
+from wand.color import Color
+from wand.image import Image
+
+
+def arg_parsing():
+    """Parses the arguments given to the script and returns them."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output", required=True)
+    parser.add_argument("--width", type=int, required=True)
+    parser.add_argument("--square", action='store_true')
+    parser.add_argument("files", nargs='+', default=[])
+
+    return parser.parse_args()
+
+
+def resize_picture(source: Image, output: str, extension: str, width: int, square: bool):
+    with source.clone() as img:
+        if square is True:
+            width = float(img.width)
+            height = float(img.height)
+            border_height = 0
+            border_width = 0
+            if width > height:
+                crop_size = int(width)
+                border_height = int(ceil((width - height) / 2))
+            else:
+                crop_size = int(height)
+                border_width = int(ceil((height - width) / 2))
+            img.border(color=Color('white'), height=border_height, width=border_width)
+            img.crop(top=0, left=0, width=crop_size, height=crop_size)
+
+        img.transform(resize=f'{width}x')
+        img.save(filename=f'{output}{extension}')
+
+        return img.format, img.width, img.height
+
+
+def open_and_resize_picture(filepath: str):
+    print(f"Resizing {filepath}")
+    filename, file_extension = os.path.splitext(os.path.basename(filepath))
+    output_path = f'{args.output}/{filename}'
+    with Image(filename=filepath) as picture:
+        picture.auto_orient()
+        resize_picture(picture, output_path, file_extension, args.width, args.square)
+        print(f"Done resizing {filepath}")
+
+
+args = arg_parsing()
+if not os.path.isdir(args.output):
+    print(f'ERROR: {args.output} is not a folder')
+    exit(1)
+
+
+for path in args.files:
+    threading.Thread(target=open_and_resize_picture(path))
